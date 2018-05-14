48241f5388e94e5454039a8be66493dab145edb5
Guillaume Pansier <guillaume.pansier@gmail.com>
2018-05-14 18:17:49 +0200

    fix bug layout Firefox

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -13,7 +13,7 @@ export class AboutmeComponent implements OnInit {
 
   // needs a dummy value, because angular material would add css
   // at comple time, when timeEventGroup is not yet defined, which breaks the layout
-  timedEventGroups: { category: string, events: TimedEvent[] }[] = [ { category: 'dummy', events: []} ];
+  timedEventGroups: { category: string, events: TimedEvent[] }[] = [ { category: 'Work Experience', events: [ { date: 'just now', title: 'Loading events'}]} ];
 
   constructor(private eventService: EventService) { }
 

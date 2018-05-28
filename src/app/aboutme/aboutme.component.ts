fc5a95eac2a33c899239659e76afe0cdc0e936ff
Guillaume Pansier <guillaume.pansier@gmail.com>
2018-05-28 20:49:34 +0200

    make aboutme page responsive

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { TimedEvent } from './timed-event/timed-event';
 import { Http, Headers, Response } from '@angular/http';
 import { EventService } from './event.service';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 
 @Component({
@@ -9,7 +10,10 @@ import { EventService } from './event.service';
   templateUrl: './aboutme.component.html',
   styleUrls: ['./aboutme.component.css']
 })
-export class AboutmeComponent implements OnInit {
+export class AboutmeComponent implements OnInit, OnDestroy {
+
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
 
   // needs a dummy value, because angular material would add css
   // at comple time, when timeEventGroup is not yet defined, which breaks the layout
@@ -17,8 +21,10 @@ export class AboutmeComponent implements OnInit {
     { category: 'Work Experience', events: [ { date: 'just now', title: 'Loading events'}]}
   ];
 
-  constructor(private eventService: EventService) {
-
+  constructor(private eventService: EventService, media: MediaMatcher, changeDetectorRef: ChangeDetectorRef) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
   ngOnInit() {
@@ -27,5 +33,9 @@ export class AboutmeComponent implements OnInit {
         this.timedEventGroups = data;
       }
     );
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 }

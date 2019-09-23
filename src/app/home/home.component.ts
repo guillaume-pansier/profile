c57567322c54f69f8fdabc48c4516c6e7f4d7a53
Guillaume Pansier <guillaume.pansier@gmail.com>
2019-09-23 12:47:42 +0200

    SEO opti
    rename images to mention guillaume pansier
    add title to <a> elements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  title = 'Guillaume Pansier';
 
   constructor(private _http: HttpClient) {}
 
